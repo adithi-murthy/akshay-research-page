543a93f26dd7800b509804c08f22b79eb401448e
Adithi M <Adithi@wirelessprv-10-193-201-244.near.illinois.edu>
2022-08-10 00:55:18 -0400

    responsive navbar changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,13 +2,29 @@ import React from "react";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
 
+const styles = {
+  sameline: {
+    display: "flex",
+    justifyContent: "spaceBetween",
+  },
+  drophash: {
+    marginTop: "7.5%",
+  },
+};
+
 const NavBar = () => {
   return (
     <>
       <Navbar sticky="top" expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">Homepage Name</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            style={{
+              backgroundImage:
+                "url('data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 30 30%27%3e%3cpath stroke=%27rgba%28255, 255, 255, 0.55%29%27 stroke-linecap=%27round%27 stroke-miterlimit=%2710%27 stroke-width=%272%27 d=%27M4 7h22M4 15h22M4 23h22%27/%3e%3c/svg%3e')",
+            }}
+          />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <HashLink smooth to="/#about-me" className="hashlink">
@@ -17,36 +33,49 @@ const NavBar = () => {
               <HashLink smooth to="/#education" className="hashlink">
                 Education
               </HashLink>
-              <HashLink smooth to="/#research" className="hashlink">
-                Research
-              </HashLink>
-              <NavDropdown
-                title=""
-                id="collasible-nav-dropdown"
-                className="dropdown"
-              >
-                <NavDropdown.Item href="/research-1">
-                  Research1
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/research-2">
-                  Research2
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/research-3">
-                  Research3
-                </NavDropdown.Item>
-              </NavDropdown>
-              <HashLink smooth to="/#publications" className="hashlink">
-                Publications
-              </HashLink>
-              <NavDropdown
-                title=""
-                id="collasible-nav-dropdown"
-                className="dropdown"
-              >
-                <NavDropdown.Item href="/publications">
-                  List of All Publications
-                </NavDropdown.Item>
-              </NavDropdown>
+              <div style={styles.sameline}>
+                <HashLink
+                  smooth
+                  to="/#research"
+                  className="hashlink drophash"
+                >
+                  Research
+                </HashLink>
+                <NavDropdown
+                  title=""
+                  id="collasible-nav-dropdown"
+                  className="dropdown"
+                >
+                  <NavDropdown.Item href="/research-1">
+                    Research1
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/research-2">
+                    Research2
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/research-3">
+                    Research3
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              <div style={styles.sameline}>
+                <HashLink
+                  smooth
+                  to="/#publications"
+                  className="hashlink"
+                  style={styles.drophash}
+                >
+                  Publications
+                </HashLink>
+                <NavDropdown
+                  title=""
+                  id="collasible-nav-dropdown"
+                  className="dropdown"
+                >
+                  <NavDropdown.Item href="/publications">
+                    List of All Publications
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
             </Nav>
             <Nav>
               <NavDropdown
@@ -64,17 +93,10 @@ const NavBar = () => {
                   Research3
                 </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link
-                href="/publications"
-                className="hashlink"
-              >
+              <Nav.Link href="/publications" className="hashlink">
                 Publication List
               </Nav.Link>
-              <HashLink
-                smooth
-                to="/#contact-me"
-                className="contact-me"
-              >
+              <HashLink smooth to="/#contact-me" className="contact-me">
                 Contact Me
               </HashLink>
             </Nav>
