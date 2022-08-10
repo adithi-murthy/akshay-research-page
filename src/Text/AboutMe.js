20ec466501ef52622061d3a574b2ac5aa2f405ba
Akshay Murthy <aamurthy26@gmail.com>
2022-08-09 22:06:57 -0500

    name added

--- src/Text/AboutMe.js
+++ src/Text/AboutMe.js
@@ -1,5 +1,5 @@
-export const Name = "MyFirstName MyLastName"
-export const JobTitle = "Researcher at Company"
+export const Name = "Akshay Murthy"
+export const JobTitle = "Researcher at Fermi National Accelerator Laboratory"
 export const AboutMeText =
   "Pulled from the about me text page: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at faucibus turpis. Pellentesque tempor rutrum dignissim. Quisque egestas luctus commodo. Vestibulum congue convallis quam, eu rhoncus felis sodales quis. Suspendisse tincidunt quis urna at tristique. Vestibulum sempereuismod dapibus. Donec placerat nisl vehicula erat tempor dapibus. Aliquamporttitor purus sem, ut ultrices enim placerat eget. Nulla vitae mifaucibus, auctor massa in, auctor augue. Morbi eu est pharetra, mollis leoquis, porttitor erat. Orci varius natoque penatibus et magnis disparturient montes, nascetur ridiculus mus. Donec in orci a nisi rhoncus tempor et et dolor. Quisque lectus nunc, condimentum eu leo id, pulvinar aliquet massa. Sed bibendum sapien sit amet ex luctus, et finibus enim fermentum. Fusce vitae turpis mollis, suscipit sapien sed, sodales elit.";
 export const CVLink = "https://illinois.edu"
