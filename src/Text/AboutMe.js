b0bc00643214f77f9c2d0158994f6df931ad64f4
Akshay Murthy <aamurthy26@gmail.com>
2022-08-11 10:17:48 -0500

    minor_edits

--- src/Text/AboutMe.js
+++ src/Text/AboutMe.js
@@ -1,5 +1,5 @@
 export const Name = "Akshay Murthy"
-export const JobTitle = "Researcher at Fermi National Accelerator Laboratory"
+export const JobTitle = "Associate Scientist at Fermi National Accelerator Laboratory"
 export const AboutMeText =
 "I am an Associate Scientist at Fermilab investigating superconducting resonators and transmons through the use of electron microscopy and other advanced materials characterization techniques. I previously obtained my PhD in Materials Science and Engineering at Northwestern University. My thesis research focused on studying two-dimensional materials for next generation electronic device applications.";
 export const CVLink = "https://illinois.edu"
