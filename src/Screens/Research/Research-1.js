4da89439f65dfe3f24f05804ae2a60d8e4a1a6e9
Adithi M <Adithi@wirelessprv-10-193-201-244.near.illinois.edu>
2022-08-28 19:31:30 -0500

    In situ Processing of Quantum Materials research page formatted and responsive

--- src/Screens/Research/Research-1.js
+++ src/Screens/Research/Research-1.js
@@ -6,35 +6,17 @@ import Research1Figure from "../../assets/Figure1.png";
 import "./Research.css";
 import "../Home.css";
 
-const styles = {};
 const Research1 = () => {
   return (
     <div>
       <div className="researchDiv">
-        {/* <div> */}
-        {/* <img src={picture} alt="Me" id="aboutMeImg" /> */}
         <img src={Research1Figure} id="research1Figure" />
-        {/* <div id="aboutMeText">{AboutMe}</div> */}
-        <br></br>
-        <p id="research1Text">{Research1Text}</p>
+        <div id="research1Text">{Research1Text}</div>
       </div>
-      {/* </div> */}
-
-      <h3 style={{ margin: "2.5% 0% 0% 0%" }}>Related Publications:</h3>
+      <h3 style={{ margin: "2.5% 0% 0% 5%" }}>Related Publications:</h3>
       <ol>
-        <li>
-          {/* <a href="https://arxiv.org/abs/2203.08710" className="link"> */}
-          {Research1Publication1}
-          {/* </a> */}
-        </li>
-        {/* 1.{" "}
-      <a href="https://arxiv.org/abs/2203.08710" className="link">
-      {Research1Publication1}
-    </a> */}
-        {/* 2.{" "} */}
-        {/* <a href="https://aip.scitation.org/doi/full/10.1063/5.0079321" className="link"> */}
+        <li>{Research1Publication1}</li>
         <li>{Research1Publication2}</li>
-        {/* </a> */}
       </ol>
     </div>
   );
--- src/Screens/Research/Research-2.js
+++ src/Screens/Research/Research-2.js
@@ -5,14 +5,18 @@ import Research2Figure from "../../assets/Figure2.png";
 const styles = {};
 const Research2 = () => {
   return(
-  <div>
     <div>
-    <img src={Research2Figure} id="Research2Figure" /> 
+      <div className="researchDiv">
+        <img src={Research2Figure} id="research2Figure" />
+        <div id="research2Text">{Research2Text}</div>
+      </div>
+      <h3 style={{ margin: "2.5% 0% 0% 5%" }}>Related Publications:</h3>
+      <ol>
+        <li>{}</li>
+        <li>{}</li>
+      </ol>
     </div>
-    <div>
-    <div style={{ marginLeft: "30%" }}>{Research2Text}</div>
-    </div>
-  </div>)
+  )
 };
 
 export default Research2;
