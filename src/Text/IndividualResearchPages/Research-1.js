d099fb337d4f013415d13a8d579ae4b0dead2520
Adithi M <Adithi@wirelessprv-10-193-201-244.near.illinois.edu>
2022-08-28 19:06:21 -0500

    Added HTML links to publications

--- src/Text/IndividualResearchPages/Research-1.js
+++ src/Text/IndividualResearchPages/Research-1.js
@@ -1,13 +1,25 @@
-const Research1Text =
-  "Understanding of Local Structure on Global Scale: Semiconductor electronics represent a hallmark example of materials understanding and control. Through decades of dedicated materials research focusing on the deep connections between structure, processing, properties, and performance, manufacturers are able to reliably control semiconductor materials down to the parts-per-billion chemical levels and nanometer length scales necessary to achieve stable behavior in the current generation of transistors. As device miniaturization approaches the limit of diminishing returns, quantum computing has emerged as the next frontier and a platform for potentially addressing computational problems deemed intractable with classical computing. In the case of this platform as well, the quality of the constituent materials and associated surfaces and interfaces dictates the device performance and stability. Through the use of state-of-the-art analytical electron microscopy and diffraction methods, I am interested in identifying and quantifying vacancies, grain boundaries, and topological defects in crystalline and amorphous quantum materials. Further, I apply bulk measurement techniques such as x-ray techniques, mass spectrometry techniques, vibrational spectroscopy techniques, and scanning probe microscopy techniques to obtain a comprehensive understanding of defect densities within the material.";
-const Research1Publication12 = (
+const Research1Text = (
   <div>
-    <b>Murthy, A. A.</b>; Das, P. M.; Ribet, S. M.; Kopas, C.; Lee, J.; Reagor,
-    M. J.; Zhou, L.; Kramer, M. J.; Hersam, M. C.; Checchin, M.; Grassellino,
-    A.; dos Reis, R.; Dravid, V. P.; Romanenko, A., Developing a Chemical and
-    Structural Understanding of the Surface Oxide in a Niobium Superconducting
-    Qubit. <i>arXiv preprint arXiv:2203.08710</i>, <b>2022</b>{" "}
-    <a href="https://arxiv.org/abs/2203.08710">[HTML]</a>
+    <h1>Understanding of Local Structure on Global Scale:</h1>
+    Semiconductor electronics represent a hallmark example of materials
+    understanding and control. Through decades of dedicated materials research
+    focusing on the deep connections between structure, processing, properties,
+    and performance, manufacturers are able to reliably control semiconductor
+    materials down to the parts-per-billion chemical levels and nanometer length
+    scales necessary to achieve stable behavior in the current generation of
+    transistors. As device miniaturization approaches the limit of diminishing
+    returns, quantum computing has emerged as the next frontier and a platform
+    for potentially addressing computational problems deemed intractable with
+    classical computing. In the case of this platform as well, the quality of
+    the constituent materials and associated surfaces and interfaces dictates
+    the device performance and stability. Through the use of state-of-the-art
+    analytical electron microscopy and diffraction methods, I am interested in
+    identifying and quantifying vacancies, grain boundaries, and topological
+    defects in crystalline and amorphous quantum materials. Further, I apply
+    bulk measurement techniques such as x-ray techniques, mass spectrometry
+    techniques, vibrational spectroscopy techniques, and scanning probe
+    microscopy techniques to obtain a comprehensive understanding of defect
+    densities within the material.
   </div>
 );
 
@@ -18,6 +30,17 @@ const Research1Publication1 = (
     A.; dos Reis, R.; Dravid, V. P.; Romanenko, A., Developing a Chemical and
     Structural Understanding of the Surface Oxide in a Niobium Superconducting
     Qubit. <i>arXiv preprint arXiv:2203.08710</i>, <b>2022</b>{" "}
+    <a href="https://arxiv.org/abs/2203.08710">[HTML]</a>
+  </div>
+);
+
+const Research1Publication19 = (
+  <div>
+    <b>Murthy, A. A.</b>; Das, P. M.; Ribet, S. M.; Kopas, C.; Lee, J.; Reagor,
+    M. J.; Zhou, L.; Kramer, M. J.; Hersam, M. C.; Checchin, M.; Grassellino,
+    A.; dos Reis, R.; Dravid, V. P.; Romanenko, A., Developing a Chemical and
+    Structural Understanding of the Surface Oxide in a Niobium Superconducting
+    Qubit. <i>arXiv preprint arXiv:2203.08710</i>, <b>2022</b>
   </div>
 );
 
@@ -26,7 +49,8 @@ const Research1Publication2 = (
     <b>Murthy, A. A.</b>; Lee, J.; Kopas, C.; Reagor, M. J.; McFadden, A. P.;
     Pappas, D. P.; Checchin, M.; Grassellino, A.; Romanenko, A., TOF-SIMS
     analysis of decoherence sources in superconducting qubits.{" "}
-    <i>App. Phys. Lett.</i> 120 (4), <b>2022</b>, 044002;
+    <i>App. Phys. Lett.</i> 120 (4), <b>2022</b>, 044002{" "}
+    <a href="https://aip.scitation.org/doi/full/10.1063/5.0079321">[HTML]</a>
   </div>
 );
 export { Research1Text, Research1Publication1, Research1Publication2 };
