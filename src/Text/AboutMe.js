5297a00f9b7c47a83aac160da8ad6556442f4ef7
Akshay Murthy <aamurthy26@gmail.com>
2022-09-21 16:31:55 -0500

    Press_releases

--- src/Text/AboutMe.js
+++ src/Text/AboutMe.js
@@ -2,7 +2,7 @@ export const Name = "Akshay Murthy"
 export const JobTitle = "Associate Scientist at Fermi National Accelerator Laboratory"
 export const AboutMeText =
 "I am an Associate Scientist at Fermilab investigating superconducting resonators and transmons through the use of electron microscopy and other advanced materials characterization techniques. I previously obtained my PhD in Materials Science and Engineering at Northwestern University. My thesis research focused on studying two-dimensional materials for next generation electronic device applications.";
-export const Publication1 = "Developing a Chemical and Structural Understanding of the Surface Oxide in a Niobium Superconducting Qubit"
-export const Publication2 = "TOF-SIMS analysis of decoherence sources in superconducting qubits"
-export const Publication1Link = "https://https://doi.org/10.1021/acsnano.2c07913"
-export const Publication2Link = "https://aip.scitation.org/doi/full/10.1063/5.0079321"
+export const Publication1 = "New SQMS study uncovers impurities in a qubit"
+export const Publication2 = "Atomic Dance Reveals New Insights into Performance of 2D Materials"
+export const Publication1Link = "https://news.fnal.gov/2022/02/new-sqms-study-uncovers-impurities-in-a-qubit/?utm_source=newsletter&utm_medium=email&utm_campaign=ft-220217"
+export const Publication2Link = "https://www.mccormick.northwestern.edu/news/articles/2020/02/atomic-dance-reveals-new-insights-into-performance-of-2d-materials.html"
