4f20d0fb85cd8eb2fa8ad8e67e75638277375eab
Akshay Murthy <aamurthy26@gmail.com>
2024-12-24 16:35:00 -0600

    updating publications. is this thing working?

--- src/Text/AboutMe.js
+++ src/Text/AboutMe.js
@@ -1,8 +1,10 @@
 export const Name = "Akshay Murthy"
-export const JobTitle = "Associate Scientist at Fermi National Accelerator Laboratory"
+export const JobTitle = "Group Leader at Fermi National Accelerator Laboratory"
 export const AboutMeText =
-"I am an Associate Scientist at Fermilab investigating superconducting resonators and transmons through the use of electron microscopy and other advanced materials characterization techniques. I previously obtained my PhD in Materials Science and Engineering at Northwestern University. My thesis research focused on studying two-dimensional materials for next generation electronic device applications.";
-export const Publication1 = "New SQMS study uncovers impurities in a qubit"
-export const Publication2 = "Atomic Dance Reveals New Insights into Performance of 2D Materials"
-export const Publication1Link = "https://news.fnal.gov/2022/02/new-sqms-study-uncovers-impurities-in-a-qubit/?utm_source=newsletter&utm_medium=email&utm_campaign=ft-220217"
-export const Publication2Link = "https://www.mccormick.northwestern.edu/news/articles/2020/02/atomic-dance-reveals-new-insights-into-performance-of-2d-materials.html"
+"I am an associate scientist and the group leader of the Materials for Quantum Devices group within the Superconducting Quantum Materials and Systems Division at Fermilab, part of the Department of Energy’s National Quantum Information Science Research Center — SQMS. The research group primarily focuses on improving the length of time information can be effectively stored and processed in a superconducting quantum device. Dramatically extending this coherence time is critical for realizing next-generation quantum computers and sensors";
+export const Publication1 = "Researchers at the SQMS Center achieve leading performance in transmon qubits"
+export const Publication2 = "New SQMS study uncovers impurities in a qubit"
+export const Publication3 = "SQMS researchers discover performance-limiting nanohydrides in superconducting qubits"
+export const Publication1Link = "https://news.fnal.gov/2024/05/researchers-at-the-sqms-center-achieve-leading-performance-in-transmon-qubits/"
+export const Publication2Link = "https://news.fnal.gov/2022/02/new-sqms-study-uncovers-impurities-in-a-qubit/?utm_source=newsletter&utm_medium=email&utm_campaign=ft-220217"
+export const Publication3Link = "https://news.fnal.gov/2021/12/sqms-researchers-discover-performance-limiting-nanohydrides-in-superconducting-qubits/"
